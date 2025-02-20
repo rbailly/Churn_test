838fb75059ee22d8251638d3c4bd890673d2766b
Rodolphe Bailly <rodolphe1970@gmail.com>
2025-02-20 12:02:28 +0100

    test - first commit from visual studio

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify, render_template
 import joblib
 import numpy as np 
 
-##Charger le modèle
+###Charger le modèle
 model = joblib.load('data/logreg_model.pkl')
 
 # Initialiser l'application Flask
